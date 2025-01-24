bade3cea04457e0969ed9567687bec52b31dcb67
SwapnilKumbhar7 <156442590+SwapnilKumbhar7@users.noreply.github.com>
2025-01-24 12:40:15 +0530

    Delete VIS directory

--- VIS/in/bushansirgur/vis/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package in.bushansirgur.vis.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-  extends AbstractSecurityWebApplicationInitializer {
-
-}
